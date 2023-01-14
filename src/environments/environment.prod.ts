1724e32d95982f0d703d6f70592d1464111ff7d3
karazma <kkeyanece2128@gmail.com>
2023-01-14 19:07:18 +0530

    Increasing the space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDOt2qnfUHdg6VDHg55aYTsb9XlfR9CQAs',
+    authDomain: 'lemellowauthentication.firebaseapp.com',
+    databaseURL: 'https://testDB.firebaseio.com',
+    projectId: 'lemellowauthentication',
+    storageBucket: 'lemellowauthentication.appspot.com',
+    messagingSenderId: '277001891658',
+    appId: '1:277001891658:web:14e129ec967d9c11835bb3',
+    measurementId: "G-B9GTXCMKS9"
+  }
 };
